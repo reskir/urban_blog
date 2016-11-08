e2b7681cabe900f71b24b0c93994aaa5cf50c2ce
Kiril Abaškin <kiril.abaskin@adform.com>
2016-11-08 22:26:57 +0200

    updates

---
+++ assets/js/load.js
@@ -0,0 +1,6 @@
+(function(){
+    document.querySelectorAll('.wrapper')[1].style.opacity = "0.1";
+    window.onload = function(){
+        document.querySelectorAll('.wrapper')[1].style.opacity = "1";
+    };
+})();
